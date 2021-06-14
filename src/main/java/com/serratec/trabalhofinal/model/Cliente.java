24fa90a10365c0d25838b4e79bfcee80b94a9861
Matheusfariasc <81425135+Matheusfariasc@users.noreply.github.com>
2021-06-13 21:47:05 -0300

    AAtualizando com o cpf

--- src/main/java/com/serratec/trabalhofinal/model/Cliente.java
+++ src/main/java/com/serratec/trabalhofinal/model/Cliente.java
@@ -185,4 +185,73 @@ public class Cliente implements UserDetails {
 	public boolean isEnabled() {
 		return true;
 	}
+	
+	public class CpfCnpjUtils {
+        private final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+        private final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+		private String cpf;
+		private String cnpj;
+		
+		public String getCpf() {
+			return cpf;
+		}
+
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+
+		public String getCnpj() {
+			return cnpj;
+		}
+
+		public void setCnpj(String cnpj) {
+			this.cnpj = cnpj;
+		}
+
+        public boolean isValid(String cpfCnpj) {
+        	this.cpf=cnpj;
+            return (isValidCPF(cpfCnpj) || isValidCNPJ(cpfCnpj));
+        }
+
+        private int calcularDigito(String str, int[] peso) {
+            int soma = 0;
+            for (int indice=str.length()-1, digito; indice >= 0; indice-- ) {
+                digito = Integer.parseInt(str.substring(indice,indice+1));
+                soma += digito*peso[peso.length-str.length()+indice];
+            }
+            soma = 11 - soma % 11;
+            return soma > 9 ? 0 : soma;
+        }
+
+        private String padLeft(String text, char character) {
+            return String.format("%11s", text).replace(' ', character);
+        }
+
+        private boolean isValidCPF(String cpf) {
+        	this.cpf=cpf;
+            cpf = cpf.trim().replace(".", "").replace("-", "");
+            if ((cpf==null) || (cpf.length()!=11)) return false;
+
+            for (int j = 0; j < 10; j++)
+                if (padLeft(Integer.toString(j), Character.forDigit(j, 10)).equals(cpf))
+                    return false;
+
+            Integer digito1 = calcularDigito(cpf.substring(0,9), pesoCPF);
+            Integer digito2 = calcularDigito(cpf.substring(0,9) + digito1, pesoCPF);
+            return cpf.equals(cpf.substring(0,9) + digito1.toString() + digito2.toString());
+        }
+
+        private boolean isValidCNPJ(String cnpj) {
+        	this.cpf=cnpj;
+            cnpj = cnpj.trim().replace(".", "").replace("-", "");
+            if ((cnpj==null)||(cnpj.length()!=14)) return false;
+
+            Integer digito1 = calcularDigito(cnpj.substring(0,12), pesoCNPJ);
+            Integer digito2 = calcularDigito(cnpj.substring(0,12) + digito1, pesoCNPJ);
+            return cnpj.equals(cnpj.substring(0,12) + digito1.toString() + digito2.toString());
+        }
+
+		
+    }
+    
 }
